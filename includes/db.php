4a437871108aaa9a64b8b142b26b07b123a14258
freethemesandapps <freethemesandapps@gmail.com>
2016-02-29 14:12:22 -0500

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php 
 $DB_HOST = 'localhost';
-$DB_USER = 'andrade_shoplist';
-$DB_PASS = 'shop123';
-$DB_NAME = 'andrade_shoplist';
+$DB_USER = 'username';
+$DB_PASS = 'password';
+$DB_NAME = 'database_name';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
